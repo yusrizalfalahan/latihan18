92e38a6c1967d2b151e43d3bff5dd3132d4c7b45
yusrizalfalahan <yusrizalfalahan27@gmail.com>
2018-10-01 17:31:36 +0700

    riza1l commit

---
+++  latihan18/src/pbo2/pkg10117043/latihan18/kelipatan/PBO210117043Latihan18kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117043.latihan18.kelipatan;
+
+/**
+ *NAMA                 : Yusrizal Falahan
+ * Kelas                : PBO-2 
+ * NIM                  : 10117043
+ * Deskripsi Program    : Program ini berisi perintah untuk menghitung bilangan
+ *                        kelipatan 3,5 dimulai dari 3,5 sampai dengan 35
+ * @author Acer
+ */
+public class PBO210117043Latihan18kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       double bilangan;
+               bilangan = 0;
+       while (bilangan <35){
+           bilangan = bilangan + 3.5;
+           System.out.println(bilangan);
+        }
+    }
+    
+}
